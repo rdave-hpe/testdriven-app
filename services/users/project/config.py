07019437bd99d3708615595aa844a11141499541
rdave-hpe <raunak.dave@hpe.com>
2019-12-12 09:49:39 +0530

    Second Commit

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,15 +1,19 @@
+import os
+
 class BaseConfig(object):
     "Base Configuration"
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DevelopmentConfig(BaseConfig):
     "Development Configuration"
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestingConfig(BaseConfig):
     "Test Configuration"
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
 
 class ProductionConfig(BaseConfig):
     "Production Configuration"
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
